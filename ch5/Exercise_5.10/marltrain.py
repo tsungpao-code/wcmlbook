4392411ff6edc19df47ff961639485b0423c406a
Huang Kai <50916916+huangkk99@users.noreply.github.com>
2025-06-09 19:35:33 +0800

    Update marltrain.py

--- ch5/Exercise_5.10/marltrain.py
+++ ch5/Exercise_5.10/marltrain.py
@@ -11,6 +11,7 @@ from replay_memory import ReplayMemory
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+
 # ################## SETTINGS ######################
 up_lanes = [i/2.0 for i in [3.5/2, 3.5/2 + 3.5, 250+3.5/2, 250+3.5+3.5/2, 500+3.5/2, 500+3.5+3.5/2]]
 down_lanes = [i/2.0 for i in [250-3.5-3.5/2, 250-3.5/2, 500-3.5-3.5/2, 500-3.5/2, 750-3.5-3.5/2, 750-3.5/2]]
@@ -18,14 +19,14 @@ left_lanes = [i/2.0 for i in [3.5/2, 3.5/2 + 3.5, 433+3.5/2, 433+3.5+3.5/2, 866+
 right_lanes = [i/2.0 for i in [433-3.5-3.5/2, 433-3.5/2, 866-3.5-3.5/2, 866-3.5/2, 1299-3.5-3.5/2, 1299-3.5/2]]
 width = 750/2
 height = 1298/2
-label = 'marl_model'
-n_veh = 4
-n_neighbor = 1
+label = 'marl_model4_3_0_1'
+n_veh = 4 # For the number of the vehicles in each direction remain same, n_veh % 4 == 0
+n_neighbor = 3
 n_RB = n_veh
 # Environment Parameters
 env = Environment_marl.Environ(down_lanes, up_lanes, left_lanes, right_lanes, width, height, n_veh, n_neighbor)
 env.new_random_game()   # initialize parameters in env
-n_episode = 3000
+n_episode = 30
 n_step_per_episode = int(env.time_slow/env.time_fast)
 epsi_final = 0.02
 epsi_anneal_length = int(0.8*n_episode)
@@ -38,7 +39,7 @@ n_hidden_3 = 120
 ######################################################
 
 
-def get_state(env, idx=(0, 0), ind_episode=1., epsi=0.02):
+def get_state(env, idx=(0, 0), ind_episode=1., epsi=0.02, n=0.):
     """ Get state from the environment """
 
     # V2I_channel = (env.V2I_channels_with_fastfading[idx[0], :] - 80) / 60
@@ -55,11 +56,12 @@ def get_state(env, idx=(0, 0), ind_episode=1., epsi=0.02):
     load_remaining = np.asarray([env.demand[idx[0], idx[1]] / env.demand_size])
     time_remaining = np.asarray([env.individual_time_limit[idx[0], idx[1]] / env.time_slow])
 
-    return np.concatenate((V2I_fast, np.reshape(V2V_fast, -1), V2V_interference, np.asarray([V2I_abs]), V2V_abs, time_remaining, load_remaining, np.asarray([ind_episode, epsi])))
+    return np.concatenate((V2I_fast, np.reshape(V2V_fast, -1), V2V_interference, np.asarray([V2I_abs]), V2V_abs,
+                           time_remaining, load_remaining, np.asarray([ind_episode, epsi])))
 
 
 n_input_size = len(get_state(env=env))
-n_output_size = n_RB  # Choose Band
+n_output_size = n_RB * len(env.V2V_power_dB_List)
 
 
 class DQN(nn.Module):
@@ -82,138 +84,53 @@ class DQN(nn.Module):
         return x
 
 
-class Actor(nn.Module):
-    def __init__(self, state_dim, action_dim, max_power):
-        super(Actor, self).__init__()
-        self.l1 = nn.Linear(state_dim, 500)
-        self.l1.weight.data.normal_(0, 0.1)
-        self.l2 = nn.Linear(500, 250)
-        self.l2.weight.data.normal_(0, 0.1)
-        self.l3 = nn.Linear(250, 120)
-        self.l3.weight.data.normal_(0, 0.1)
-        self.l4 = nn.Linear(120, action_dim)
-        self.l4.weight.data.normal_(0, 0.1)
-        self.max_power = max_power
-        self.bias_ = 38  # Make the power in [-100, 23]dbm
-
-    def forward(self, x):
-        x = F.relu(self.l1(x))
-        x = F.relu(self.l2(x))
-        x = F.relu(self.l3(x))
-        x = self.max_power * torch.tanh(self.l4(x)) - self.bias_
-        return x
-
-
-class Critic(nn.Module):
-    def __init__(self, state_dim, action_dim):
-        super(Critic, self).__init__()
-        self.l1 = nn.Linear(state_dim + action_dim, 500)
-        self.l1.weight.data.normal_(0, 0.1)
-        self.l2 = nn.Linear(500, 250)
-        self.l2.weight.data.normal_(0, 0.1)
-        self.l3 = nn.Linear(250, 120)
-        self.l3.weight.data.normal_(0, 0.1)
-        self.l4 = nn.Linear(120, 1)
-        self.l4.weight.data.normal_(0, 0.1)
-
-    def forward(self, s, a):
-        x = torch.cat([s, a], 1)
-        x = F.relu(self.l1(x))
-        x = F.relu(self.l2(x))
-        x = F.relu(self.l3(x))
-        x = self.l4(x)
-        return x
-
-
-class DDPG_Agent:
+class Agent:
     def __init__(self, memory_entry_size):
         self.discount = 1
+        self.double_q = True
         self.memory_entry_size = memory_entry_size
         self.memory = ReplayMemory(self.memory_entry_size)
-
         self.model = DQN(n_input_size, n_hidden_1, n_hidden_2, n_hidden_3, n_output_size).to(device)
-        self.target_model = DQN(n_input_size, n_hidden_1, n_hidden_2, n_hidden_3, n_output_size).to(device)
+        # if torch.cuda.device_count()>1:
+        #    self.model = nn.DataParallel(self.model)
+        # self.model.to(device)
+        self.target_model = DQN(n_input_size, n_hidden_1, n_hidden_2, n_hidden_3, n_output_size).to(device)  # Target Model
         self.target_model.eval()
         self.optimizer = torch.optim.RMSprop(self.model.parameters(), lr=0.001, momentum=0.05, eps=0.01)
-
-        self.actor = Actor(n_input_size, 1, env.max_power).to(device)
-        self.actor_target = Actor(n_input_size, 1, env.max_power).to(device)
-        self.actor_target.eval()
-        self.actor_target.load_state_dict(self.actor.state_dict())
-        self.actor_optimizer = torch.optim.Adam(self.actor.parameters(), lr=0.0001)
-
-        self.critic = Critic(n_input_size, 1).to(device)
-        self.critic_target = Critic(n_input_size, 1).to(device)
-        self.critic_target.eval()
-        self.critic_target.load_state_dict(self.critic.state_dict())
-        self.critic_optimizer = torch.optim.Adam(self.critic.parameters(), lr=0.001)
-
         self.loss_func = nn.MSELoss()
-        self.tau = 0.001
 
-    def predict_band(self, s_t, ep=0.):
-        if random.random() > ep:
+    def predict(self, s_t, ep=0.):
+        n_power_levels = len(env.V2V_power_dB_List)
+        # state_t = torch.from_numpy(s_t).type(torch.float32).view([1, self.memory_entry_size])
+        if torch.rand(1) > ep:
             with torch.no_grad():
                 q_values = self.model(torch.tensor(s_t, dtype=torch.float32).unsqueeze(0).to(device))
                 return q_values.max(1)[1].item()
         else:
-            return random.choice(range(n_RB))
+            return random.choice(range(n_power_levels * n_RB))
 
-    def predict_power(self, s_t):
-        with torch.no_grad():
-            power_value = self.actor(torch.tensor(s_t, dtype=torch.float32).unsqueeze(0).to(device))
-            return power_value.item()
-
-    def Q_Learning_mini_batch(self):
-
-        batch_s_t, batch_s_t_plus_1, batch_action_band, batch_action_power, batch_reward = self.memory.sample()
-
-        band = torch.LongTensor(batch_action_band).to(device)
-        power = torch.FloatTensor(batch_action_power).unsqueeze(1).to(device)
+    def Q_Learning_mini_batch(self):  # Double Q-Learning
+        batch_s_t, batch_s_t_plus_1, batch_action, batch_reward = self.memory.sample()
+        action = torch.LongTensor(batch_action).to(device)
         reward = torch.FloatTensor(batch_reward).to(device)
         state = torch.FloatTensor(np.float32(batch_s_t)).to(device)
         next_state = torch.FloatTensor(np.float32(batch_s_t_plus_1)).to(device)
-        # Update DQN
-        next_q_value = self.target_model(next_state).max(1)[0]
-        expected_q_value = reward + self.discount * next_q_value
+        if self.double_q:
+            next_action = self.model(next_state).max(1)[1]
+            next_q_values = self.target_model(next_state)
+            next_q_value = next_q_values.gather(1, next_action.unsqueeze(1)).squeeze(1)
+            expected_q_value = reward + self.discount * next_q_value
+        else:
+            next_q_value = self.target_model(next_state).max(1)[0]
+            expected_q_value = reward + self.discount * next_q_value
         q_values = self.model(state)
-        q_acted = q_values.gather(1, band.unsqueeze(1)).squeeze(1)
+        q_acted = q_values.gather(1, action.unsqueeze(1)).squeeze(1)
         loss = self.loss_func(expected_q_value.detach(), q_acted)
         # backward and optimize
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
-
-        # Update Actor
-        a = self.actor_target(state)
-        q = self.critic_target(state, a)
-        actor_loss = - torch.mean(q)
-        self.actor_optimizer.zero_grad()
-        actor_loss.backward()
-        self.actor_optimizer.step()
-
-        # Update Critic
-        reward1 = reward.unsqueeze(1)
-        a_target = self.actor_target(next_state)
-        q_tmp = self.critic_target(next_state, a_target)
-        # print("a_target:", a_target.size(), "q_tmp", q_tmp.size())
-        q_target = reward1 + self.discount * q_tmp
-        q_eval = self.critic(state, power)
-        # print("reward1:", reward1.size(), "q_target:", q_target.size(), "q_eval:", q_eval.size())
-        critic_loss = self.loss_func(q_target.detach(), q_eval)
-        self.critic_optimizer.zero_grad()
-        critic_loss.backward()
-        self.critic_optimizer.step()
-
-        # softly update the target networks
-        for x in self.actor_target.state_dict().keys():
-            eval('self.actor_target.' + x + '.data.mul_((1-self.tau))')
-            eval('self.actor_target.' + x + '.data.add_(self.tau * self.actor.' + x + '.data)')
-        for x in self.critic_target.state_dict().keys():
-            eval('self.critic_target.' + x + '.data.mul_((1-self.tau))')
-            eval('self.critic_target.' + x + '.data.add_(self.tau * self.critic.' + x + '.data)')
-
-        return loss.item(), actor_loss.item(), critic_loss.item()
+        return loss.item()
 
     def update_target_network(self):
         self.target_model.load_state_dict(self.model.state_dict())
@@ -225,20 +142,12 @@ class DDPG_Agent:
             os.makedirs(os.path.dirname(model_path))
         torch.save(self.model.state_dict(), model_path+'.ckpt')
         torch.save(self.target_model.state_dict(), model_path+'_t.ckpt')
-        torch.save(self.actor.state_dict(), model_path + '_actor.ckpt')
-        torch.save(self.actor_target.state_dict(), model_path + '_actor_t.ckpt')
-        torch.save(self.critic.state_dict(), model_path + '_critic.ckpt')
-        torch.save(self.critic_target.state_dict(), model_path + '_critic_t.ckpt')
 
     def load_models(self, model_path):
         current_dir = os.path.dirname(os.path.realpath(__file__))
         model_path = os.path.join(current_dir, "model/" + model_path)
         self.model.load_state_dict(torch.load(model_path + '.ckpt'))
         self.target_model.load_state_dict(torch.load(model_path + '_t.ckpt'))
-        self.actor.load_state_dict(torch.load(model_path + '_actor.ckpt'))
-        self.actor_target.load_state_dict(torch.load(model_path + '_actor_t.ckpt'))
-        self.critic.load_state_dict(torch.load(model_path + '_critic.ckpt'))
-        self.critic_target.load_state_dict(torch.load(model_path + '_critic_t.ckpt'))
 
 
 # ----------------------------------------------------------------------------
@@ -246,11 +155,11 @@ print(device)
 agents = []
 for ind_agent in range(n_veh * n_neighbor):  # initialize agents
     print("Initializing agent", ind_agent)
-    agent = DDPG_Agent(memory_entry_size=len(get_state(env)))
+    agent = Agent(memory_entry_size=len(get_state(env)))
     agents.append(agent)
 # ----------------------------Training----------------------------------------
-record_reward = np.zeros([n_episode, 1])
-#  record_loss = []
+record_reward = np.zeros([n_episode*n_step_per_episode, 1])
+record_loss = []
 for i_episode in range(n_episode):
     if i_episode < epsi_anneal_length:
         epsi = 1 - i_episode * (1 - epsi_final) / (epsi_anneal_length - 1)  # epsilon decreases over each episode
@@ -269,50 +178,44 @@ for i_episode in range(n_episode):
     for i_step in range(n_step_per_episode):
         time_step = i_episode * n_step_per_episode + i_step
         state_old_all = []
-        action_band_list = []
-        action_power_list = []
-        action_all_band = np.zeros([n_veh, n_neighbor], dtype='int32')
-        action_all_power = np.zeros([n_veh, n_neighbor], dtype='float64')
+        action_all = []
+        action_all_training = np.zeros([n_veh, n_neighbor, 2], dtype='int32')
         for i in range(n_veh):
             for j in range(n_neighbor):
-                state = get_state(env, [i, j], i_episode / (n_episode - 1), epsi)
+                state = get_state(env, [i, j], i_episode / (n_episode - 1), epsi, i * n_neighbor + j)
                 state_old_all.append(state)
-                action_band = agents[i * n_neighbor + j].predict_band(state, epsi)
-                action_power = agents[i * n_neighbor + j].predict_power(state)
-                action_band_list.append(action_band)
-                action_power_list.append(action_power)
+                action = agents[i * n_neighbor + j].predict(state, epsi)
+                action_all.append(action)
 
-                action_all_band[i, j] = action_band  # chosen RB
-                action_all_power[i, j] = action_power  # power level
+                action_all_training[i, j, 0] = action % n_RB  # chosen RB
+                action_all_training[i, j, 1] = int(np.floor(action / n_RB))  # power level
 
         # All agents take actions simultaneously, obtain shared reward, and update the environment.
-        action_band_temp = action_all_band.copy()
-        action_power_temp = action_all_power.copy()
-        train_reward = env.act_for_training(action_band_temp, action_power_temp)
-        record_reward[i_episode] += train_reward
+        action_temp = action_all_training.copy()
+        train_reward = env.act_for_training(action_temp)
+        record_reward[time_step] = train_reward
 
         env.renew_channels_fastfading()
-        env.Compute_Interference(action_band_temp, action_power_temp)
+        env.Compute_Interference(action_temp)
 
         for i in range(n_veh):
             for j in range(n_neighbor):
                 state_old = state_old_all[n_neighbor * i + j]
-                action_band = action_band_list[n_neighbor * i + j]
-                action_power = action_power_list[n_neighbor * i + j]
-                state_new = get_state(env, [i, j], i_episode / (n_episode - 1), epsi)
+                action = action_all[n_neighbor * i + j]
+                state_new = get_state(env, [i, j], i_episode / (n_episode - 1), epsi, i * n_neighbor + j)
                 # add entry to this agent's memory
-                agents[i * n_neighbor + j].memory.add(state_old, state_new, train_reward, action_band, action_power)
+                agents[i * n_neighbor + j].memory.add(state_old, state_new, train_reward, action)
 
                 # training this agent
                 if time_step % mini_batch_step == mini_batch_step - 1:
-                    # q-learning
-                    loss_val_batch, _, _ = agents[i * n_neighbor + j].Q_Learning_mini_batch()
-                    #  record_loss[i_episode] = loss_val_batch
-                    if i_episode % 50 == 0 and i == 0 and j == 0:
-                        print('episode:', i_episode, 'loss:', loss_val_batch, 'reward:', record_reward[i_episode])
+                    loss_val_batch = agents[i * n_neighbor + j].Q_Learning_mini_batch()
+                    record_loss.append(loss_val_batch)
+                    if i_episode % 100 == 0 and i == 0 and j == 0:
+                        print('Episode:', i_episode, 'agent0_loss', loss_val_batch)
                 if time_step % target_update_step == target_update_step - 1:
                     agents[i * n_neighbor + j].update_target_network()
-
+                    # if i == 0 and j == 0:
+                    #    print('Update target Q network...')
 print('Training Done. Saving models...')
 for i in range(n_veh):
     for j in range(n_neighbor):
@@ -323,7 +226,6 @@ current_dir = os.path.dirname(os.path.realpath(__file__))
 reward_path = os.path.join(current_dir, "model/" + label + '/reward.mat')
 scipy.io.savemat(reward_path, {'reward': record_reward})
 
-# record_loss = np.asarray(record_loss).reshape((-1, n_veh * n_neighbor))
-# loss_path = os.path.join(current_dir, "model/" + label + '/train_loss.mat')
-# scipy.io.savemat(loss_path, {'train_loss': record_loss})
-
+record_loss = np.asarray(record_loss).reshape((-1, n_veh * n_neighbor))
+loss_path = os.path.join(current_dir, "model/" + label + '/train_loss.mat')
+scipy.io.savemat(loss_path, {'train_loss': record_loss})
